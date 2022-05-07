cf5ccaa3d00e48f1d5d9f484831cacbb3070db23
Jason Asselin <asselin.jason@gmail.com>
2022-05-06 20:42:29 -0400

    config telescope

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -6,14 +6,19 @@ local keymap = require 'lib.utils'.keymap
 telescope.setup {
     defaults = { 
         file_ignore_patterns = {"node_modules", ".git/"},
+        layout_config = {
+            vertical = { width = 0.9, preview_height = 0.65 },
+        },
+        layout_strategy = "vertical",
         mappings = {
             i = {
                 ['<esc>'] = actions.close,
                 ['<C-j>'] = actions.move_selection_next,
                 ['<C-k>'] = actions.move_selection_previous,
-            },
-        }
-    } 
+                ["<C-h>"] = "which_key"
+            }
+        },
+    }
 }
 
 vim.api.nvim_create_user_command(
